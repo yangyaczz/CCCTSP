3db4057805359ca3fc034185f4166ca19b35586b
yyczz <a729746954@gmail.com>
2024-05-18 02:22:34 +0800

    init

---
+++ src/test.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+
+contract A {
+
+}
